671ef44771b1878ff560dd53daff6b0fcbb5925c
Gurditt Rai <gurdittrai@hotmail.com>
2018-04-21 09:04:12 -0400

    added boxs for crop if needed

--- readPlate.py
+++ readPlate.py
@@ -11,10 +11,13 @@ h = np.size(rawimg, 0)
 w = np.size(rawimg, 1)
 
 x = (int)(w * 0.05)
-y = (int)(h * 0.2)
+y = (int)(h * 0.25)
 w = (int)(w - x)
 h = (int)(h - y)
 cropimg = rawimg[y:h, x:w]
+
+# save offset
+rawoffset = (x,y)
 
 # convert grayscale
 grayimg = cv2.cvtColor(cropimg, cv2.COLOR_RGB2GRAY)
@@ -22,7 +25,7 @@ if grayimg is None:
     print("error opening file\n")
 
 # seperate background
-thresh = 65
+thresh = 100
 maxval = 255
 th, dst = cv2.threshold(grayimg, thresh, maxval, cv2.THRESH_BINARY)
 cv2.imshow('raw', rawimg)
@@ -33,12 +36,17 @@ _, contours, hierarchy = cv2.findContours(dst, cv2.RETR_TREE, cv2.CHAIN_APPROX_S
 
 contour_list = []
 for contour in contours:
+    [x, y, w, h] = cv2.boundingRect(contour)
     area = cv2.contourArea(contour)
-    if area > 150 :
+    if area > 500 :
+        cv2.rectangle(cropimg, (x, y), (x + w, y + h), (255, 0, 255), 2)
+
+    if area > 200 :
         contour_list.append(contour)
 
-cv2.drawContours(rawimg, contour_list,  -1, (255,180,0), 2)
-cv2.imshow('Objects Detected', rawimg)
+cv2.drawContours(rawimg, contour_list,  -1, (0,180,255), 2, offset=rawoffset)
+cv2.imshow('Objects Detected Contour', rawimg)
+cv2.imshow('Objects Detected Box', cropimg)
 
 # wait for user to exit
 cv2.waitKey(0) 
