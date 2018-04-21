7ca8b9a2a030d68606adfb8919776258ae0f6a32
Gurditt Rai <gurdittrai@hotmail.com>
2018-04-21 07:11:18 -0400

    contour

--- readPlate.py
+++ readPlate.py
@@ -1,22 +1,44 @@
 # Reading Car Plate Numbers
 import cv2
+import numpy as np
 
-# Get Image and convert grayscale
+# get image
 fp = "car-plate-sample-1.jpg"
-grayimg = cv2.imread(fp, cv2.IMREAD_GRAYSCALE)
+rawimg = cv2.imread(fp)
+
+# crop
+h = np.size(rawimg, 0)
+w = np.size(rawimg, 1)
+
+x = (int)(w * 0.05)
+y = (int)(h * 0.2)
+w = (int)(w - x)
+h = (int)(h - y)
+cropimg = rawimg[y:h, x:w]
+
+# convert grayscale
+grayimg = cv2.cvtColor(cropimg, cv2.COLOR_RGB2GRAY)
 if grayimg is None:
     print("error opening file\n")
 
 # seperate background
-thresh = 35
+thresh = 65
 maxval = 255
 th, dst = cv2.threshold(grayimg, thresh, maxval, cv2.THRESH_BINARY)
-
-# create window and show the gray image
-cv2.namedWindow("binary", cv2.WINDOW_AUTOSIZE)
+cv2.imshow('raw', rawimg)
 cv2.imshow("binary", dst)
 
-cv2.imwrite("result.jpg", dst) 
+# get contour
+_, contours, hierarchy = cv2.findContours(dst, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+contour_list = []
+for contour in contours:
+    area = cv2.contourArea(contour)
+    if area > 150 :
+        contour_list.append(contour)
+
+cv2.drawContours(rawimg, contour_list,  -1, (255,180,0), 2)
+cv2.imshow('Objects Detected', rawimg)
 
 # wait for user to exit
 cv2.waitKey(0) 
